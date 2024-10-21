960d502c0219350376fcba789a4c05ae2355ac58
juvereturn@gmail.com <Rusty05151>
2024-10-21 02:41:24 -0700

    Add texture script (not implemented)

--- FALL2024/FALL2024/Square.cpp
+++ FALL2024/FALL2024/Square.cpp
@@ -4,10 +4,10 @@ Square::Square(Vector3 point1, Vector3 point2, Vector3 point3, Vector3 point4, V
 {
     vertices =
     {
-        point1.x, point1.y, point1.z, color.x, color.y, color.z, alpha,
-        point2.x, point2.y, point2.z, color.x, color.y, color.z, alpha,
-        point3.x, point3.y, point3.z, color.x, color.y, color.z, alpha,
-        point4.x, point4.y, point4.z, color.x, color.y, color.z, alpha
+        point1.x, point1.y, point1.z, color.x, color.y, color.z, alpha, //0.0f, 0.0f,
+        point2.x, point2.y, point2.z, color.x, color.y, color.z, alpha, //0.0f, 0.0f,
+        point3.x, point3.y, point3.z, color.x, color.y, color.z, alpha, //1.0f, 1.0f,
+        point4.x, point4.y, point4.z, color.x, color.y, color.z, alpha//, 1.0f, 0.0f
     };
 
     indices =
