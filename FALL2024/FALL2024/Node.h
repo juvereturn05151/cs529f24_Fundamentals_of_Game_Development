c0e6ab50dcac9e1487b58abaab1bb85443c47250
juvereturn@gmail.com <Rusty05151>
2024-12-13 15:24:18 -0800

    add more comments

--- FALL2024/FALL2024/Node.h
+++ FALL2024/FALL2024/Node.h
@@ -3,35 +3,61 @@
 /*
 Author: Ju-ve Chankasemporn
 E-mail: juvereturn@gmail.com
-Description: This is basically an empty game object. 
+Description: This is an empty game object class, which can serve as a basic building block
+             for a scene graph system, where each node can have a transform and child nodes.
 */
 
-#include"Matrix4.h"
+#include "Matrix4.h"
 #include <vector>
 #include "Transform.h"
-#include"Mesh.h"
+#include "Mesh.h"
+
 class Node
 {
 protected:
-    Transform *transform;
+    // The transformation (position, rotation, scale) of this node
+    Transform* transform;
+
+    // The parent node of this node, if any
     Node* parent = nullptr;
+
+    // A list of child nodes that are attached to this node
     std::vector<Node*> children;
+
+    // Sets the parent node, ensuring the parent-child relationship is maintained
     void setParent(Node* parent);
+
+    // Retrieves the global transformation matrix for this node, including the transformations of the parent chain
     Matrix4<float> getGlobalModelMatrix() const;
+
+    // Indicates whether this node is active in the scene (can be updated and drawn)
     bool isActive = true;
 
 public:
+    // Constructor: Initializes a new node with default values
     Node();
+
+    // Destructor: Cleans up the node and its resources
     virtual ~Node();
 
-    // Get access to the Transform object
-    Transform *getTransform();
+    // Retrieves the Transform object associated with this node
+    Transform* getTransform();
+
+    // Sets whether the node is active or not
     void setIsActive(bool active);
+
+    // Gets the current activation status of the node
     bool getIsActive();
 
+    // Updates the node (can be overridden by derived classes for custom update logic)
     virtual void update(float deltaTime);
+
+    // Draws the node (can be overridden by derived classes for custom drawing logic)
     virtual void draw();
 
+    // Adds a child node to this node's list of children
     void addChild(Node* child);
+
+    // Retrieves the global position of this node, taking into account the entire transformation hierarchy
     Vector3 getGlobalPosition() const;
 };
--- FALL2024/FALL2024/Transform.h
+++ FALL2024/FALL2024/Transform.h
@@ -1,28 +1,54 @@
 #pragma once
-#include"Vector3.h"
+#include "Vector3.h"
 #include "Matrix4.h"
+
+/*
+Author: Ju-ve Chankasemporn
+E-mail: juvereturn@gmail.com
+Description: Transform component containing position, rotation, and scale of an object.
+             This class is used to define the transformation properties of a node in a scene.
+*/
 
 class Transform
 {
 private:
-	Vector3 position;
-	Vector3 rotation;
-	Vector3 scale;
+    // The position of the object in 3D space
+    Vector3 position;
+
+    // The rotation of the object (in degrees or radians) around each axis
+    Vector3 rotation;
+
+    // The scale of the object in 3D space, affecting its size
+    Vector3 scale;
+
 public:
-	Transform();
-	~Transform();
+    // Constructor: Initializes the position, rotation, and scale to default values
+    Transform();
 
-	void setPosition(const Vector3& position);
-	Vector3 getPosition() const;
+    // Destructor: Cleans up any resources (if needed)
+    ~Transform();
 
-	void setRotation(const Vector3& rotation);
-	Vector3 getRotation() const;
+    // Sets the position of the object
+    void setPosition(const Vector3& position);
 
-	void setScale(const Vector3& scale);
-	Vector3 getScale() const;
+    // Retrieves the current position of the object
+    Vector3 getPosition() const;
 
-	Matrix4<float> getModelMatrix() const;
+    // Sets the rotation of the object
+    void setRotation(const Vector3& rotation);
 
-	void translate(const Vector3& translateAmount);
+    // Retrieves the current rotation of the object
+    Vector3 getRotation() const;
+
+    // Sets the scale of the object
+    void setScale(const Vector3& scale);
+
+    // Retrieves the current scale of the object
+    Vector3 getScale() const;
+
+    // Returns the model matrix that combines position, rotation, and scale for this object
+    Matrix4<float> getModelMatrix() const;
+
+    // Translates (moves) the object by a certain amount in 3D space
+    void translate(const Vector3& translateAmount);
 };
-
